e66c2f16e0eefb2ad797eedf8e73f48c38195219
DipeshChouhan <49608738+DipeshChouhan@users.noreply.github.com>
2020-06-10 15:35:38 +0530

    Update CArray.c
    Uses calloc to allocate memory which automatically assigned all bits with zero. No requirement to assigned with zero manually.

--- data_structures/Array/CArray.c
+++ data_structures/Array/CArray.c
@@ -36,12 +36,8 @@ void swap(CArray *array, int position1, int position2);
 CArray * getCArray(int size)
 {
 	CArray *array = (CArray *) malloc(sizeof(CArray));
-	array->array = (int *) malloc(sizeof(int) * size);
-	array->size = size;
-	int i;
-	for (i = 0; i < size; i++) {
-		array->array[i] = 0;
-	}
+	array->array = (int *) calloc(sizeof(int) * size);
+	array->size = size;}
 	return array;
 }
 
